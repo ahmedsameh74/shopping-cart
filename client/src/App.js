c99d999dab6a47d18723d6a16688ba70b0cfa243
ahmed sameh <asameh1500@gmail.com>
2022-05-21 17:43:17 +0200

    header comp

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,13 @@
+import Header from "./components/header/Header";
+
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="layout">
+      <Header />
+      <main>content</main>
+      <footer>footer</footer>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/components/header/Header.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "../../css/header/Header.css";
+
+export default function Header() {
+  return <header>shopping cart</header>;
+}
