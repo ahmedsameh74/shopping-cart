1f49038e8560392c1d6d27241f8a26f110cc9c52
ahmed sameh <asameh1500@gmail.com>
2022-05-22 17:42:38 +0200

    animition-2

--- client/src/components/checkout/Checkout.js
+++ client/src/components/checkout/Checkout.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../../css/checkout/Checkout.css";
+import Zoom from "react-reveal/Zoom";
 
 export default function Checkout(props) {
   return (
@@ -7,29 +8,31 @@ export default function Checkout(props) {
       <span className="close" onClick={props.closeForm}>
         &times;
       </span>
-      <form onSubmit={(e) => props.submitOrder(e)}>
-        <div>
-          <label>Name</label>
-          <input
-            type="text"
-            required
-            name="name"
-            onChange={(e) => props.handleChange(e)}
-          />
-        </div>
-        <div>
-          <label>Email</label>
-          <input
-            type="email"
-            required
-            name="email"
-            onChange={(e) => props.handleChange(e)}
-          />
-        </div>
-        <div>
-          <button type="submit">submit</button>
-        </div>
-      </form>
+      <Zoom left>
+        <form onSubmit={(e) => props.submitOrder(e)}>
+          <div>
+            <label>Name</label>
+            <input
+              type="text"
+              required
+              name="name"
+              onChange={(e) => props.handleChange(e)}
+            />
+          </div>
+          <div>
+            <label>Email</label>
+            <input
+              type="email"
+              required
+              name="email"
+              onChange={(e) => props.handleChange(e)}
+            />
+          </div>
+          <div>
+            <button type="submit">submit</button>
+          </div>
+        </form>
+      </Zoom>
     </>
   );
 }
